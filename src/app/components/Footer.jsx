6e58c0ff647663666ec46ef148341a4dedd079d2
Dalia Santini <santini.dg@hotmail.com>
2025-09-17 16:39:41 -0600

    Add Behance Link to Footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,8 +1,20 @@
 export default function Footer() {
   return (
     <footer className='flex justify-center items-center text-xs text-tertiary h-16 px-4 border-t pt-1 pb-2'>
-      <p className='text-[11px] text-center'>
-        Developed by Dalia Santini © 2025
+      <p>
+        {' '}
+        Developed by{' '}
+        <span>
+          <a
+            href='https://www.behance.net/dalisanti'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='underline hover:text-accent transition-colors duration-150'
+          >
+            Dalia Santini
+          </a>
+        </span>{' '}
+        © 2025
       </p>
     </footer>
   );
