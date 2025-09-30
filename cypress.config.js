cfd37fdb7d08997dbb69ffa7c49aa6f1b3a00d16
Dalia Santini <santini.dg@hotmail.com>
2025-09-30 14:46:58 -0600

    update cypress.config,js code and cypress folder structure

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,18 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/routes.cy.js',
+    baseUrl:
+      process.env.CYPRESS_BASE_URL || 'https://montserratlanding.vercel.app',
     setupNodeEvents(on, config) {
-      baseUrl: process.env.CYPRESS_BASE_URL ||
-        'https://montserratlanding.vercel.app';
-      // implement node event listeners here
+      return config;
+    },
+  },
+  component: {
+    specPattern: 'cypress/components.cy.js',
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
     },
   },
 });
